5df6a3957c9e2d06f3ce36e059ea7ec0967c181c
kemad97 <kerolos.emad992@gmail.com>
2025-04-27 11:23:23 +0300

    Initial Commit

---
+++ Day2 swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day2 swift
+//
+//  Created by Kerolos on 27/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
